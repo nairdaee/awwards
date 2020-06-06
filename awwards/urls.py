a6d7066fa1e852884856663f750922cecf819ef5
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 17:03:21 +0300

    add main project urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,4 +4,7 @@ from django.contrib.auth import views
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('rating.urls')),
+    url(r'^accounts/',include('accounts.urls')),
+    url(r'^api-token-auth/', obtain_auth_token) 
 ]
