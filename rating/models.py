da8fed0e32c84cb20913c0a4097584295c6fb5f8
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 17:07:00 +0300

    add class for project categories

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class categories(models.Model):
+    categories= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categories
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def delete_category(cls,categories):
+        cls.objects.filter(categories=categories).delete()
+
+
