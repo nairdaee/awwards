fb4dc57a90ff8c9e648f2249786e3552655e00dd
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 19:25:56 +0300

    add project function...debug and configure to return the accurate overall ratings of the users' projects

--- rating/views.py
+++ rating/views.py
@@ -74,3 +74,63 @@ def search(request):
     else:
         message = "Please enter search term"
         return render(request, 'search.html', {"message":message, "projects":projects,'profile':profile})
+
+@login_required(login_url='/accounts/login')
+def project(request, project_id):
+    current_user = request.user
+    profile =Profile.objects.get(user=current_user)
+    message = "Thank you for voting"
+    try:
+        project = Project.objects.get(id=project_id)
+    except Project.DoesNotExist:
+        raise ObjectDoesNotExist()
+
+   
+    total_design = 0
+    total_usability = 0
+    total_creativity = 0
+    total_content = 0
+    overall_score = 0
+
+    ratings = Rating.objects.filter(project=project_id)
+    if len(ratings) > 0:
+        users = len(ratings)
+    else:
+        users = 1
+    
+    design = list(Rating.objects.filter(project=project_id).values_list('design',flat=True))
+    usability = list(Rating.objects.filter(project=project_id).values_list('usability',flat=True))
+    creativity = list(Rating.objects.filter(project=project_id).values_list('creativity',flat=True))
+    content = list(Rating.objects.filter(project=project_id).values_list('content',flat=True))
+    
+    
+    total_design=sum(design)/users
+    total_usability=sum(usability)/users
+    total_creativity=sum(creativity)/users
+    total_content=sum(content)/users
+
+
+    overall_score=(total_design+total_content+total_usability+total_creativity)/4
+
+    project.design = total_design
+    project.usability = total_usability
+    project.creativity = total_creativity
+    project.content = total_content
+    project.overall = overall_score
+
+    project.save()
+
+    
+    if request.method == 'POST':
+        form = RatingForm(request.POST, request.FILES) 
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.project= project
+            rating.profile = profile
+            if not Rating.objects.filter(profile=profile, project=project).exists():
+                rating.overall_score = (rating.design+rating.usability+rating.creativity+rating.content)/4
+                rating.save()
+    else:
+        form = RatingForm()
+    return render(request, "project.html",{"project":project,"profile":profile,"ratings":ratings,"form":form, "message":message, 'total_design':total_design, 'total_usability':total_usability, 'total_creativity':total_creativity, 'total_content':total_content})
+
