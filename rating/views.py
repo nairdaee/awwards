d722b8cdad253eda841914c580f48e7b6465ae8c
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 19:17:36 +0300

    add profile view function to views

--- rating/views.py
+++ rating/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render,redirect
-from .models import Project, Profile, Rating, categories, technologies
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
+from .models import Project, Profile, Rating, categories, technologies
+from .forms import ProfileForm, UploadForm, RatingForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer, ProjectSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+from django.core.exceptions import ObjectDoesNotExist
+
 
 @login_required(login_url='/accounts/login')
 def index(request):
@@ -15,3 +24,10 @@ def index(request):
         return redirect('edit')
     return render(request, 'index.html', locals())
 
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(user=current_user)
+    projects = Project.objects.filter(user=current_user)
+    my_profile = Profile.objects.get(user=current_user)
+    return render(request, 'profile.html', locals())
