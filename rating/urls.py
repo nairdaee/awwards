617a8f2fcb5c1f0becc129913cc48cf87f435fa4
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 19:40:36 +0300

    add url patterns for the rating app views

--- rating/urls.py
+++ rating/urls.py
@@ -5,4 +5,13 @@ from . import views
 
 urlpatterns=[
     url('^$', views.index, name = 'index'),
+    url(r'^myprofile$',views.profile,name='myprofile'),
+    url(r'^profile/edit$',views.edit_profile,name='edit'),
+    url(r'^project/new$', views.new_project, name='post'),
+    url(r'^project/(?P<project_id>[0-9])$',views.project,name='project'),
+    url(r'^search/', views.search,name='search'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'api/projects/project-id/(?P<pk>[0-9]+)/$', views.ProjectDescription.as_view()),
+    url(r'api/profiles/profile-id/(?P<pk>[0-9]+)/$', views.ProfileDescription.as_view())
 ]
