c47c0dd0fc3525f78f9ad59a58733069f9bb9fe7
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 17:09:17 +0300

    add rating,project and profile classes

--- rating/models.py
+++ rating/models.py
@@ -27,3 +27,46 @@ class technologies(models.Model):
     def delete_technology(cls,technologies):
         cls.objects.filter(technologies=technologies).delete()
         
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    landing_page = models.ImageField(upload_to='landing/')
+    description = models.CharField(max_length=255)
+    live_link = models.CharField(max_length=255)
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall = models.IntegerField(blank=True,default=0)
+    categories = models.ManyToManyField(categories)
+    technologies = models.ManyToManyField(technologies)
+    posted  = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_project(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profilepic = models.ImageField(upload_to='profiles/')
+    bio = models.CharField(max_length=255)
+    prefname = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.prefname
+
+class Rating(models.Model):
+    CHOICES = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
+
+    design = models.IntegerField(choices=CHOICES,blank=True,default=0)
+    usability = models.IntegerField(choices=CHOICES, blank=True,default=0)
+    creativity = models.IntegerField(choices=CHOICES, blank=True,default=0)
+    content = models.IntegerField(choices=CHOICES, blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
