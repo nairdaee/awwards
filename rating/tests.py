c602ea2f7ec9b94caea7ce4c7dc6508457a6528c
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 17:10:37 +0300

    add tests for the models created

--- rating/tests.py
+++ rating/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import categories,technologies, Profile, Project
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your tests here.
+class categoriesTestCase(TestCase):
+    def setUp(self):
+        self.Test = categories(categories='Test')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Test,categories))
+
+    def tearDown(self):
+        categories.objects.all().delete()
+
+    def test_save_method(self):
+        self.Test.save_category()
+        category = categories.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_method(self):
+        self.Test.delete_category('Test')
+        category = categories.objects.all()
+        self.assertTrue(len(category)==0)
+
+class technologiesTestCase(TestCase):
+    def setUp(self):
+        self.Tester = technologies(technologies='Tester')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Tester,technologies))
+
+    def tearDown(self):
+        technologies.objects.all().delete()
+
+    def test_save_method(self):
+        self.Tester.save_technology()
+        technology = technologies.objects.all()self.Test = categories(categories='Test')
+        self.assertTrue(len(technology)>0)
+
+    def test_delete_method(self):
+        self.Tester.delete_technology('Tester')
+        technology = technologies.objects.all()
+        self.assertTrue(len(technology)==0)
