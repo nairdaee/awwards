99cda5a1d2c876b1268348841b1cc325bbf5163d
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 19:42:15 +0300

    register models to admin.py

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Profile, Rating, technologies, categories
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('technologies', 'categories')
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Rating)
+admin.site.register(categories)
+admin.site.register(technologies)
