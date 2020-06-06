ed31547b0ae31008766cd02feed74be52426a7cd
Adrian Etenyi <etadriano2@gmail.com>
2020-06-06 19:19:42 +0300

    add view function for editing user profile

--- rating/views.py
+++ rating/views.py
@@ -31,3 +31,17 @@ def profile(request):
     projects = Project.objects.filter(user=current_user)
     my_profile = Profile.objects.get(user=current_user)
     return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = current_user
+            prof.save()
+            return redirect('myprofile')
+    else:
+        form = ProfileForm()
+    return render(request, 'edit_profile.html', {'form': form, 'profile':profile})
